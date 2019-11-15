4555d0c727a09137535ce13c406ed2d69f778e36
talha <talha.6862@gmail.com>
2019-11-16 04:13:42 +0500

    navbar almost ready

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
+// changing background color of navbar on scroll
 var myNav = document.getElementById("myNav")
 var logo = document.getElementById("logo")
+
 window.onscroll = function () {
     if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
         logo.style.height = '50px'
         myNav.classList.add("nav-colored")
         myNav.classList.remove("nav-transparent");
     }
-    else {
+    else if(window.screen.width > 425){
         logo.style.height = '70px'
         myNav.classList.add("nav-transparent");
         myNav.classList.remove("nav-colored");
     }
 }
+
+// hamburger-icon
+$(document).ready(function(){
+    $('#btnControl').click(function(){
+        $('#hamburger-icon').click(function(){
+            $(this).toggleClass('open');
+            $('#mobile-nav-ul').toggleClass('open-nav');
+        });
+    });
+});
+
+
+// resizing intro video to 16:9
+window.onload = function videoResize(){
+    var introVidHeight = document.getElementById("intro-video-div").offsetWidth*(9/16)
+    document.getElementById("intro-video-div").style.height = introVidHeight + 'px'
+}
+window.onresize = function videoResize(){
+    var introVidHeight = document.getElementById("intro-video-div").offsetWidth*(9/16)
+    document.getElementById("intro-video-div").style.height = introVidHeight + 'px'
+}
+
